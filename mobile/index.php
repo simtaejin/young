85c8c5ae33d4fbc7bcafd92b23b39369d4ff412f
taejin7937 <taejin7937@gmail.com>
2020-10-23 17:46:47 +0900

    모바일 수정

--- mobile/index.php
+++ mobile/index.php
@@ -4,84 +4,67 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once(G5_MOBILE_PATH.'/head.php');
 
 ?>
-
-    <script src="<?php echo G5_JS_URL; ?>/swipe.js"></script>
-    <script src="<?php echo G5_JS_URL; ?>/shop.mobile.main.js"></script>
-
-<!--    <div>-->
-<!--        <ul class="hbox-menu">-->
-<!--            <li><a href="http://google.com" target="_blank">google</a></li>-->
-<!--            <li><a href="http://naver.com" target="_blank">naver</a></li>-->
-<!--            <li><a href="http://daum.net" target="_blank">daum</a></li>-->
-<!--            <li><a href="http://nate.com" target="_blank">nate</a></li>-->
-<!--            <li><a href="http://zum.co.kr" target="_blank">zum</a></li>-->
-<!--            <li><a href="http://www.youtube.com" target="_blank">youtube</a></li>-->
-<!--            <li><a href="http://www.instagram.com" target="_blank">instagram</a></li>-->
-<!--            <li><a href="http://www.facebook.com" target="_blank">facebook</a></li>-->
-<!--            <li><a href="http://twitter.com" target="_blank">twitter</a></li>-->
-<!--        </ul>-->
-<!--    </div>-->
-    <div id="category"  >
-        <div class="ct_wr" >
-            <?php
-            $mshop_ca_res1 = sql_query(get_mshop_category('', 2));
-
-            for($i=0; $mshop_ca_row1=sql_fetch_array($mshop_ca_res1); $i++) {
-                if($i == 0)
-                    echo '<ul class="cate" style="">'.PHP_EOL;
-                ?>
-                <li class="cate_li_1" style="text-align: center; width: 100%; " >
-                <a href="<?php echo shop_category_url($mshop_ca_row1['ca_id']); ?>" class="cate_li_1_a"><?php echo get_text($mshop_ca_row1['ca_name']); ?></a>
-                <?php
-                if (!empty($mshop_ca_row1['ca_1'])) {
-                    $limits = " limit 0, " . $mshop_ca_row1['ca_1'];
-                } else {
-                    $limits = "";
-                }
-
-                $mshop_ca_res2_sql = " select * from `{$g5['g5_shop_item_table']}` where it_use = '1' and ca_id like '{$mshop_ca_row1['ca_id']}%'  order by it_order, it_id desc". $limits;
-                $mshop_ca_res2_result = sql_query($mshop_ca_res2_sql);
-                $ii = 0;
-                for($j=0; $mshop_ca_row2=sql_fetch_array($mshop_ca_res2_result); $j++) {
-
-                    if($j == 0)
-                        echo '<ul class=" sub_cate1">'.PHP_EOL;
-
-                        $item_link = $mshop_ca_row2['it_brand'] ? $mshop_ca_row2['it_brand'] : $mshop_ca_row2['it_origin'];
-                    ?>
-                    <li class="cate_li_2" style="float:left; width: 33.3%">
-                        <a href="<?php echo $item_link; ?>" target="_blank"><?php echo $mshop_ca_row2['it_name']; ?></a>
-                    </li>
-                    <?php
-
-
-                    $ii++;
-                }
-                if ($ii%3 > 0) {
-                    for ($y=0; $y<3-($ii%3); $y++) {
-                        ?>
-                        <li class="cate_li_2" style="float:left;width: 33.3%; " >
-                            <a href="" target="_blank">&nbsp;</a>
-                        </li>
-                        <?php
-                    }
-                }
-
-                if($j > 0)
-                    echo '</ul>'.PHP_EOL;
-                ?>
-                </li>
-                <?php
-            }
-            echo '</ul>'.PHP_EOL;
-            ?>
+    <div class="header">
+        <div class="container">
+            <a href="/">2020 신규 웹하드 순위 TOP 12</a>
         </div>
     </div>
+    <div class="container">
+        <div class="row pl-0 pr-0">
+            <?php
+            $ca_id = "mi";
+            $sql = " select * from {$g5['g5_shop_category_table']} where ca_id = '$ca_id' and ca_use = '1'  ";
+            $ca = sql_fetch($sql);
 
-    <script>
-        $("#container").addClass("idx-container");
-    </script>
+            // 스킨경로
+            $skin_dir = G5_SHOP_SKIN_PATH;
 
+            if($ca['ca_skin_dir']) {
+                if(preg_match('#^theme/(.+)$#', $ca['ca_skin_dir'], $match))
+                    $skin_dir = G5_THEME_PATH.'/'.G5_SKIN_DIR.'/shop/'.$match[1];
+                else
+                    $skin_dir = G5_PATH.'/'.G5_SKIN_DIR.'/shop/'.$ca['ca_skin_dir'];
+
+                if(is_dir($skin_dir)) {
+                    $skin_file = $skin_dir.'/'.$ca['ca_skin'];
+
+                    if(!is_file($skin_file))
+                        $skin_dir = G5_SHOP_SKIN_PATH;
+                } else {
+                    $skin_dir = G5_SHOP_SKIN_PATH;
+                }
+            }
+
+            // 리스트 스킨
+            $skin_file = $skin_dir.'/main.skin.php';
+
+            $order_by = 'it_order, it_id desc';
+
+            $list = new item_list($skin_file, $ca['ca_list_mod'], $ca['ca_list_row'], $ca['ca_img_width'], $ca['ca_img_height']);
+            $list->set_category($ca['ca_id'], 1);
+            $list->set_category($ca['ca_id'], 2);
+            $list->set_category($ca['ca_id'], 3);
+            $list->set_is_page(true);
+            $list->set_order_by($order_by);
+            $list->set_from_record($from_record);
+            $list->set_view('it_img', true);
+            $list->set_view('it_id', false);
+            $list->set_view('it_name', true);
+            $list->set_view('it_basic', true);
+            $list->set_view('it_cust_price', true);
+            $list->set_view('it_price', true);
+            $list->set_view('it_icon', true);
+            $list->set_view('sns', true);
+            ?>
+
+            <div class="col-md-12 m-0 p-0 mt-1 d-flex flex-wrap">
+                <?php
+                echo $list->run();
+                ?>
+            </div>
+
+        </div>
+    </div>
 
 <?php
 include_once(G5_MOBILE_PATH.'/tail.php');
